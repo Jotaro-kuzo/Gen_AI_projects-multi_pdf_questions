f64d0bea4f115527368754c41592bce61b076f0a
Jotaro-kuzo <sk6724486@gmail.com>
2024-03-05 00:10:39 +0530

    first version of LLM model to chat over multiple PDF files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Gen_AI_projects-multi_pdf_questions
 This is a model that can answer questions by taking multiple pdfs as input
+
+Used GEMINI-PRO model for LLM.
+FAISS, embedding-001 for embedding purpose
+Storing in local as of now as embedded files
---
+++ app.py
@@ -0,0 +1,102 @@
+import streamlit as st
+import os
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings,ChatGoogleGenerativeAI
+import google.generativeai as genai
+from dotenv import load_dotenv
+from langchain.prompts import PromptTemplate
+from langchain.chains.question_answering import load_qa_chain
+from langchain.vectorstores import FAISS
+#from langchain_google_genai import
+
+load_dotenv()
+genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
+
+# uploading pdfs
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for doc in pdf_docs:
+        pdf_reader = PdfReader(doc) # it willo be in the form of lists
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+    return text
+
+# dividing texts into chunks
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 10000, chunk_overlap = 1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+# converting chunks into vectors and storing
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model= "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding = embeddings)
+    vector_store.save_local('faiss_index') # storing in local instead of lack of database
+
+
+
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+    model = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.2)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+
+#taking user input
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+
+
+
+def main():
+    st.set_page_config("Chat Bot")
+    st.header("Chat with GEN-AI")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing...!"):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
